0540c82b30ae1e095c1479661a99518efbbc6234
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-22 19:16:00 -0300

    alterações foram feitas

---
+++ 2.GUI_intro/src/GUI_intro.java
@@ -0,0 +1,9 @@
+
+public class GUI_intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 4.Ramdom_Numbers/src/Ramdom_numbers.java
@@ -0,0 +1,9 @@
+
+public class Ramdom_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
