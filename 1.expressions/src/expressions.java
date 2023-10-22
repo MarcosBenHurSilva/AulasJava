e45375fe46768388e609bb5d77167706f758db43
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-22 19:45:54 -0300

    expressions

--- 1.expressions/src/expressions.java
+++ 1.expressions/src/expressions.java
@@ -2,7 +2,16 @@
 public class expressions {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// expression = operands $ operators
+		// operands = values, variables, numbers, quantity
+		// operators = + - * / %
+		
+		int friends = 10;
+		
+		friends--;
+		
+		System.out.println(friends);
 
 	}
 
