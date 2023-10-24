81c115796ed8a0b75559e2769dd57cd35b03f170
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-23 21:15:56 -0300

    1:37

--- 8.while_loop/src/while_loop.java
+++ 8.while_loop/src/while_loop.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 
 public class while_loop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// while loop = executes a block of code as long as a it's condition remains
+		// true
+
+		Scanner scanner = new Scanner(System.in);
+		String name = "";
+
+		while (name.isBlank()) {
+
+			System.out.print("Enter your name: ");
+
+			name = scanner.nextLine();
+
+		}
+
+		System.out.println("Welcome " + name);
 
 	}
 
--- 9.for_loop/src/for_loop.java
+++ 9.for_loop/src/for_loop.java
@@ -2,7 +2,15 @@
 public class for_loop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// for loop = executes a block of code a limited amount of times
+
+		for (int i = 10; i >= 0; i--) {
+
+			System.out.println(i);
+
+		}
+
+		System.out.println("Happy new year!");
 
 	}
 
