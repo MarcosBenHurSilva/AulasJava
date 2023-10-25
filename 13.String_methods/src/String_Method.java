9b5f31b23cd3a073ecb84aec2a29349c5bb29530
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-24 22:15:04 -0300

    Até a aula 17

--- 13.String_methods/src/String_Method.java
+++ 13.String_methods/src/String_Method.java
@@ -2,8 +2,22 @@
 public class String_Method {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// String = a reference data type that can store one or more characters
+		  //   reference data types have access to useful methods
+		  
+		  String name = "Marcos";
+		  
+		  //boolean result = name.equalsIgnoreCase("Marcos");
+		  //int result = name.length();
+		  //char result = name.charAt(0);
+		  //int result = name.indexOf("o");
+		  //boolean result = name.isEmpty();
+		  //String result = name.toUpperCase();
+		  //String result = name.toLowerCase();
+		  //String result = name.trim();
+		  String result = name.replace('o', 'u');
+		   
+		  System.out.println(result);
 	}
 
 }
