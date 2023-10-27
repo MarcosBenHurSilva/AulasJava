924f5812f76e15946beed104bf2a9b1955c106f0
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-26 22:53:01 -0300

    Final Keyword

--- 21.Final_Keyword/src/final_keyword.java
+++ 21.Final_Keyword/src/final_keyword.java
@@ -1,4 +1,11 @@
 
 public class final_keyword {
+	public static void main(String[] args) {
+
+		final double PI = 3.14159;
+		// PI = 4; //You can't change a final variable
+		System.out.println(PI);
+
+	}
 
 }
