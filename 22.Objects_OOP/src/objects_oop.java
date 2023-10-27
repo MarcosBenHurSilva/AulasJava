0fccbf07ee3049277441e78106fea1cbfde66c9e
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-26 22:53:54 -0300

    OOP

--- 22.Objects_OOP/src/objects_oop.java
+++ 22.Objects_OOP/src/objects_oop.java
@@ -1,4 +1,16 @@
 
 public class objects_oop {
+	public static void main(String[] args) {
 
+		Car myCar1 = new Car();
+		Car myCar2 = new Car();
+		
+		System.out.println(myCar1.make);
+		System.out.println(myCar1.model);
+		
+		myCar1.drive();
+		myCar1.brake();
+	}
 }
+
+
