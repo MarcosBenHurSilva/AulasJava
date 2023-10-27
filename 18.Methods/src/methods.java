5228fe7aefaddbb76def68ebb9c0bdbe52d75724
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-26 22:33:23 -0300

    methods

--- 18.Methods/src/methods.java
+++ 18.Methods/src/methods.java
@@ -1,4 +1,26 @@
 
 public class methods {
-
+	public static void main(String[] args) {
+	
+		// method = a block of code that is executed 
+		//whenever it is called upon
+		String title = "Mom";
+		int age = 51;
+		
+		hello(title, age);
+		
+		int x = 15;
+		int y = 20;
+		
+		System.out.println(add(x, y));
+	}
+	
+	static void hello(String title, int age) {
+		System.out.println("Hello " + title + "!");
+		System.out.println("You are " + age);
+	}
+	
+	static int add(int a, int b) {
+		return a + b;
+	}
 }
