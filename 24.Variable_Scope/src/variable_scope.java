9a535289c92406e292f606637948902f30b966c7
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-28 20:45:16 -0300

    Variable Scope

--- 24.Variable_Scope/src/variable_scope.java
+++ 24.Variable_Scope/src/variable_scope.java
@@ -2,7 +2,13 @@
 public class variable_scope {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 //local =  declared inside a method
+		 //         visible only to that method
+		  
+		 //global  = declared outside a method, but within a class
+		 //          visible to all parts of a class
+
+		  DiceRoller diceRoller = new DiceRoller();
 
 	}
 
