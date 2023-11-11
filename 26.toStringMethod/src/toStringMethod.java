2860af10782aa17668eae3e6c86c9b697298851b
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-11 17:28:34 -0300

    Aula toString

---
+++ 26.toStringMethod/src/toStringMethod.java
@@ -0,0 +1,15 @@
+
+public class toStringMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// toString() = special method that all objects inherit, 
+	    //    			that returns a string that "textually represents" an object.
+		//    			can be used both implicitly and explicitly
+		
+				  Car car = new Car();				  				  
+				  System.out.println(car.toString());				  
+				  // or				 
+				  System.out.println(car);
+	}
+}
